448a8eecdcdb7bc0ff5c637fcc9ad0b5e237eded
Wasmium Network Developers <hq@wasmium.network>
2022-06-09 18:02:29 +0300

    Feat: Add MPL license information on every file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// © Wasmium Network Developers
+
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
